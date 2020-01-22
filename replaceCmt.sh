a1aca7c662c38db66fce8660ffe28345d297fc9f
chinalienming <728385437@qq.com>
2020-01-22 22:00:34 +0800

    replace comment for ibug project

---
+++ replaceCmt.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+rootDir=~/phd_gogogo/final_defects4j
+cd ${rootDir}/allMethods/Math/
+find ./ -name "TriDiagonalTransformer.java" -type f | xargs -I {} sed -i '' 's/\/\*\*/\/\*/' {}
+find ./ -name "HessenbergTransformer.java" -type f | xargs -I {} sed -i '' 's/\/\*\*/\/\*/' {}
+find ./ -name "BiDiagonalTransformer.java" -type f | xargs -I {} sed -i '' 's/\/\*\*/\/\*/' {}
+
+find ./ -name "BinaryFunction.java" -type f | xargs -I {} sed -i '' 's/\/\*\*/\/\*/' {}
+find ./ -name "ComposableFunction.java" -type f | xargs -I {} sed -i '' 's/\/\*\*/\/\*/' {}
+
+cd ${rootDir}/allMethods/Closure/
+find ./ -name "ControlFlowAnalysis.java" -type f | xargs -I {} sed -i '' 's/\/\*\*/\/\*/' {}
+find ./ -name "NameGenerator.java" -type f | xargs -I {} sed -i '' 's/\/\*\*/\/\*/' {}
+
+find ./ -name "Fuzzer.java" -type f | xargs -I {} sed -i '' 's/\/\*\*/\/\*/' {}
+
+cd ${rootDir}/allMethods/Lang/
+find ./ -name "FastDateParser.java" -type f | xargs -I {} sed -i '' 's/\/\*\*/\/\*/' {}
